d34714d910717e97ba04806d2d8e3f588adce8a9
ayele-getu <ayelegetu44@gmail.com>
2023-02-01 16:07:43 +0300

    Typescript

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+
+import {RowElement,RowID } from './interface';
+
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
+
